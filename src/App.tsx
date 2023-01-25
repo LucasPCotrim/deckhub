20e44df357b37de9b58d96572549190bb6cb5cb8
Lucas Pereira Cotrim <lucaspcotrim@gmail.com>
2023-01-25 01:26:03 -0300

    feat: Added pages folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import GlobalStyle from './assets/Globalstyle';
-import HomePage from './Pages/HomePage';
+import HomePage from './pages/HomePage';
 
 export default function App() {
   return (
